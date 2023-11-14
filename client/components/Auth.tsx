27d4283032cc5299cbd70c6c90087f9fb4bdacbd
osawa-koki <koki@osawa.dev>
2023-11-15 00:14:24 +0900

    Cognitoを用いたクライアントサイド認証処理の基本部分の実装。

---
+++ client/components/Auth.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import AuthSignUp from './AuthSignUp'
+
+export type pageOptionEnum = 'SignUp' | 'ConfirmRegistration' | 'SignIn'
+
+export default function Auth (): React.JSX.Element {
+  const [pageOption, setPageOption] = useState<pageOptionEnum>('SignUp')
+
+  return (
+    <>
+      {((): React.JSX.Element => {
+        switch (pageOption) {
+          case 'SignUp':
+            return <AuthSignUp setPageOption={setPageOption} />
+          default:
+            throw new Error('Invalid pageOption.')
+        }
+      })()}
+    </>
+  )
+}
