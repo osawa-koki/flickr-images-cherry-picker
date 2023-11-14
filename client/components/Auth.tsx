46e0f19007eae964ead8e442c2e8a41bd527af90
osawa-koki <koki@osawa.dev>
2023-11-15 00:16:53 +0900

    small fix

--- client/components/Auth.tsx
+++ client/components/Auth.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import AuthSignUp from './AuthSignUp'
 
 export type pageOptionEnum = 'SignUp' | 'ConfirmRegistration' | 'SignIn'
 
 export default function Auth (): React.JSX.Element {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [pageOption, setPageOption] = useState<pageOptionEnum>('SignUp')
 
   return (
@@ -11,7 +11,8 @@ export default function Auth (): React.JSX.Element {
       {((): React.JSX.Element => {
         switch (pageOption) {
           case 'SignUp':
-            return <AuthSignUp setPageOption={setPageOption} />
+            // return <AuthSignUp setPageOption={setPageOption} />
+            return <></>
           default:
             throw new Error('Invalid pageOption.')
         }
