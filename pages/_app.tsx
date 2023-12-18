27c7dd4e19ced4d4639a4f4a2d38eed48dd7a1ef
osawa-koki <koki@osawa.dev>
2023-12-18 18:24:15 +0900

    認証画面の作成。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import { type AppProps } from 'next/app'
-import Head from 'next/head'
-
-import { ToastContainer } from 'react-toastify'
-
-import 'react-toastify/dist/ReactToastify.css'
+import '@aws-amplify/ui-react/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import '../styles/style.scss'
 import '../styles/menu.scss'
 
+import React, { useEffect } from 'react'
+import { type NextComponentType, type NextPageContext } from 'next'
+import { type AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { Amplify } from 'aws-amplify'
+import { generateClient } from 'aws-amplify/api'
+import { type WithAuthenticatorProps, withAuthenticator } from '@aws-amplify/ui-react'
+
+import { Alert } from 'react-bootstrap'
+
 import setting from '../setting'
 import Layout from '../components/Layout'
 
-export default function MyApp ({ Component, pageProps }: AppProps): React.JSX.Element {
+import awsconfig from '../src/aws-exports'
+
+Amplify.configure(awsconfig)
+export const graphqlClient = generateClient()
+
+function App ({ user, signOut, Component }: WithAuthenticatorProps & { pageProps: any, Component: NextComponentType<NextPageContext, any, any> }): React.JSX.Element {
+  if (user == null) {
+    return (
+      <Alert variant='danger'>
+        You are not signed in.
+      </Alert>
+    )
+  }
+
+  return (
+    <>
+      <Component user={user} signOut={signOut} />
+    </>
+  )
+}
+
+export default function MyApp ({ Component, pageProps, router }: AppProps): React.JSX.Element {
+  useEffect(() => {
+  }, [router.pathname])
+
   return (
     <>
       <Head>
@@ -27,8 +56,7 @@ export default function MyApp ({ Component, pageProps }: AppProps): React.JSX.El
         />
       </Head>
       <Layout>
-        <Component {...pageProps} />
-        <ToastContainer />
+        {withAuthenticator(App)({ Component, pageProps })}
       </Layout>
     </>
   )
