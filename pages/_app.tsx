a7182220b39cf063c6d8dc675ea721f1c4c40444
osawa-koki <koki@osawa.dev>
2023-12-18 23:48:34 +0900

    Amplifyコードの削除。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,17 @@
-import '@aws-amplify/ui-react/styles.css'
+import React, { createContext } from 'react'
+import { type AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import '../styles/style.scss'
 import '../styles/menu.scss'
 
-import React, { createContext, useEffect } from 'react'
-import { type NextComponentType, type NextPageContext } from 'next'
-import { type AppProps } from 'next/app'
-import Head from 'next/head'
-
-import { Amplify } from 'aws-amplify'
-import { type WithAuthenticatorProps, withAuthenticator } from '@aws-amplify/ui-react'
-
-import { Alert } from 'react-bootstrap'
-
 import setting from '../setting'
 import Layout from '../components/Layout'
-
-import awsconfig from '../src/aws-exports'
-
-Amplify.configure(awsconfig)
 
 export const PhotosContext = createContext({
   getGroups: () => [] as string[],
@@ -28,25 +20,9 @@ export const PhotosContext = createContext({
   savePhotos: (_group: string, _photos: string[]) => {}
 })
 
-function App ({ user, signOut, Component }: WithAuthenticatorProps & { pageProps: any, Component: NextComponentType<NextPageContext, any, any> }): React.JSX.Element {
-  if (user == null) {
-    return (
-      <Alert variant='danger'>
-        You are not signed in.
-      </Alert>
-    )
-  }
-
-  return (
-    <>
-      <Component user={user} signOut={signOut} />
-    </>
-  )
-}
-
 const getLocalStorageKey = (group: string): string => `_group:${group}`
 
-export default function MyApp ({ Component, pageProps, router }: AppProps): React.JSX.Element {
+export default function MyApp ({ Component, pageProps }: AppProps): React.JSX.Element {
   const getGroups = (): string[] => {
     const groups = Object.keys(localStorage)
     return groups.filter((group) => group.startsWith('_group:')).map((group) => group.replace(/^_group:/, ''))
@@ -75,9 +51,6 @@ export default function MyApp ({ Component, pageProps, router }: AppProps): Reac
     localStorage.setItem(getLocalStorageKey(group), JSON.stringify(photos))
   }
 
-  useEffect(() => {
-  }, [router.pathname])
-
   return (
     <>
       <Head>
@@ -91,14 +64,15 @@ export default function MyApp ({ Component, pageProps, router }: AppProps): Reac
         />
       </Head>
       <Layout>
-        <PhotosContext.Provider value={{
-          getGroups,
-          createGroup,
-          getPhotos,
-          savePhotos
-        }}>
-          {withAuthenticator(App)({ Component, pageProps })}
-        </PhotosContext.Provider>
+      <PhotosContext.Provider value={{
+        getGroups,
+        createGroup,
+        getPhotos,
+        savePhotos
+      }}>
+        <Component {...pageProps} />
+      </PhotosContext.Provider>
+        <ToastContainer />
       </Layout>
     </>
   )
